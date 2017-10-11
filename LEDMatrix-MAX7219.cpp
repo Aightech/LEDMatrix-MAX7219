6525b6e517c6b2b040dafe80ea3b8ac943f9848b
Aightech <aiiight.tech@gmail.com>
2017-10-11 13:51:55 +0200

    Update LEDMatrix-MAX7219.cpp

--- LEDMatrix-MAX7219.cpp
+++ LEDMatrix-MAX7219.cpp
@@ -13,7 +13,37 @@ LEDMatrix::LEDMatrix(byte _data, byte _load, byte _clock, byte _nbx, byte _nbx)
 	load_pin = _load;
 	clock_pin = _clock;
 	nbMatX = _nbx;
-  nbMatY = _nby;
+  	nbMatY = _nby;
+	LEDarr= new int*[nbMatY];
+	for(int i = 0; i < nbMatY; ++i)
+	{
+    		LEDarr[i] = new int[nbMatX];
+		for(int j=0;j<nbMatX;j++)
+			LEDarr[i][j]=0;
+	}
+	/* Ex: 2x3 matrixs of LEDs, each bit represent a LED
+	
+	Y
+       15 B0000 0000 	 B0000 0000 	B0000 0000 	
+       14 B0000 0000 	 B0000 0000 	B0000 0000 	
+       13 B0000 0000 	 B0000 0000 	B0000 0000 	
+       12 B0000 0000 <-- B0000 0000 <--	B0000 0000 --|	
+       11 B0000 0000 <-- B0000 0000 <--	B0000 0000 -||
+       10 B0000 0000 	 B0000 0000 	B0000 0000  ||
+	9 B0000 0000 	 B0000 0000 	B0000 0000  ||
+	8 B0000 0000 	 B0000 0000 	B0000 0000  ||
+						    ||
+	7 B0000 0000 	 B0000 0000 	B0000 0000  ||
+	6 B0000 0000 	 B0000 0000 	B0000 0000  ||
+	5 B0000 0000	 B0000 0000 	B0000 0000  || 
+	4 B0000 0000 --> B0000 0000 --> B0000 0000 -||
+	3 B0000 0000 --> B0000 0000 --> B0000 0000 --|
+	2 B0000 0000 	 B0000 0000 	B0000 0000 
+	1 B0000 0000 	 B0000 0000 	B0000 0000 
+	0 B0000 0000 	 B0000 0000 	B0000 0000
+               0	     1    	     2       X
+  
+	*/
 }
 
 void LEDMatrix::init()
@@ -28,6 +58,8 @@ void LEDMatrix::init()
 	setCommand(max7219_reg_shutdown, 0x01);    // not in shutdown mode
 	setCommand(max7219_reg_displayTest, 0x00); // no display test
 	
+	
+	
 	// empty registers, turn all LEDs off
 	clear();
 	
@@ -41,7 +73,11 @@ void LEDMatrix::setIntensity(byte intensity)
 
 void LEDMatrix::clear()
 {
-	
+	for(int i = 0; i < nbMatY;i++)
+	{
+		for(int j=0;j<nbMatX;j++)
+			LEDarr[i][j]=0;
+	}
 }
 
 void LEDMatrix::setCommand(byte command, byte value)
